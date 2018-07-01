25a819e6aab6573bf7a4fa3f61b6706108740361
Linda Ikechukwu <32891838+Linda-Ikechukwu@users.noreply.github.com>
2018-07-01 23:07:50 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -47,10 +47,10 @@ self.addEventListener('install', event => {
 
 
 //hijacking requests
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(response => {
         // Cache hit - return response
         if (response) {
           return response;
@@ -59,8 +59,7 @@ self.addEventListener('fetch', function(event) {
         //  Clone the request. 
         var fetchRequest = event.request.clone();
 
-        return fetch(fetchRequest).then(
-          function(response) {
+        return fetch(fetchRequest).then(response => {
             // Check if we received a valid response
             if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
